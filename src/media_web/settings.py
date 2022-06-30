564068d4cec3931bf9de06e4b8bfc0644c3ea923
RG1ee <niwows@yandex.ru>
2022-06-30 09:50:31 +0300

    initial commit

--- src/media_web/settings.py
+++ src/media_web/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     
     # my apps
     'apps.compositions',
+    'apps.anime',
+    'apps.ranobe',
+    'apps.manga',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = ''
+MEDIA_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
