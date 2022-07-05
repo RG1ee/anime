d503af2d03d866885921c798eeb53845dbfc69a3
RG1ee <niwows@yandex.ru>
2022-07-05 22:37:21 +0300

    add search by composition and work with models

--- src/apps/compositions/models.py
+++ src/apps/compositions/models.py
@@ -3,5 +3,11 @@ from django.db import models
 
 class Composition(models.Model):
     name = models.CharField(max_length=120, verbose_name="Title")
-    discription = models.TextField(verbose_name="Short Description")
+    description = models.TextField(verbose_name="Short Description")
     image = models.ImageField(verbose_name="Image")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Composition'
