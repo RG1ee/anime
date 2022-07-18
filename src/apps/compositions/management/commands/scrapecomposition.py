c5a6b39d710c3370cbf32da829dfe8bec7303c3b
Ruslan Korneev <kornerus@fjfi.cvut.cz>
2022-07-18 04:02:08 +0200

    change saving image from url logic

--- src/apps/compositions/management/commands/scrapecomposition.py
+++ src/apps/compositions/management/commands/scrapecomposition.py
@@ -1,26 +1,32 @@
-import requests as req
+import os.path
 
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
 from django.core.management.base import BaseCommand
+from django.template.defaultfilters import slugify
+import requests as req
 
 from apps.compositions.models import Composition
 
 
 class Command(BaseCommand):
-    help = "This command will scrape all japanise compositions from source"
+    help = "This command will scrape all japan compositions from source"
 
     def handle(self, *args, **kwargs):
         url = "https://ghibliapi.herokuapp.com/films"
+        # get all films from ghibli api
         films = req.get(url).json()
         for film in films:
+            # get title, description and image url from film
             title, description, image = film['title'], film['description'], film['image']
-            filename = f"{title.replace(' ', '_')}.jpg"
-            with open(filename, "wb") as file:
-                response = req.get(image) 
-                for chunk in response.iter_content(1024):
-                    file.write(chunk)
-            Composition.objects.create(
-                name=title, description=description,
-                image=filename
-            )
+            composition = Composition(name=title, description=description)
 
-            
+            # get image from image url
+            img_tmp = NamedTemporaryFile(delete=True)
+            for chunk in req.get(image).iter_content(1024):
+                img_tmp.write(chunk)
+            img_tmp.flush()
+
+            # save image
+            filename = f"{slugify(title)}{os.path.splitext(image)[-1]}"
+            composition.image.save(filename, File(img_tmp), save=True)
