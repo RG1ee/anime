02af395319adf5edc77d8a732a09041d4e32cefd
RG1ee <niwows@yandex.ru>
2022-07-20 22:10:31 +0300

    hid the secret key

--- src/media_web/settings.py
+++ src/media_web/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from media_web.const import MY_SECRET_KEY
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-szr8xziv(fysz20msw^0y2@1uwh#s=48pf$6ccm^pfm!9j&&y*'
+SECRET_KEY = MY_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
