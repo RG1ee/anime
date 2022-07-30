3129b58993cf87d70506521b55ba02b83665a6ce
Ruslan Korneev <kornerus@fjfi.cvut.cz>
2022-07-30 18:05:53 +0200

    action, overwrite get serializer class, get queryset, add filters

--- src/apps/compositions/api/views.py
+++ src/apps/compositions/api/views.py
@@ -1,11 +1,33 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from apps.compositions.api.serializers import CompositionSerializer
+from apps.compositions.api.serializers import CompositionSerializer, AnimeSerializer
 from apps.compositions.models import Composition
+from apps.users.api.permissions import IsStaffOrReadOnly
 
 
 class CompositionViewSet(viewsets.ModelViewSet):
     queryset = Composition.objects.all()
     serializer_class = CompositionSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsStaffOrReadOnly)
+
+    def get_queryset(self):
+        if self.action == 'anime':
+            composition = Composition.objects.get(pk=self.kwargs[self.lookup_field])
+            self.queryset = composition.anime.all()
+        return super().get_queryset()
+
+    def get_serializer_class(self):
+        if self.action == 'anime':
+            self.serializer_class = AnimeSerializer
+        return super().get_serializer_class()
+
+    @action(detail=True, methods=["GET", "POST"])
+    def anime(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            serializer = self.get_serializer(instance=self.get_queryset(), many=True)
+            return Response(serializer.data)
+
+        return Response({"message": "hello"})
--- src/media_web/settings.py
+++ src/media_web/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # third apps
     'django_extensions',
     'rest_framework',
+    'django_filters',
 
     # my apps
     'apps.compositions',
@@ -122,7 +123,8 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 # Internationalization
