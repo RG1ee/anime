52859ab828ff4ab7e9c3238467b86e062301dfc6
RG1ee <niwows@yandex.ru>
2022-08-10 14:02:53 +0300

    refactoring, add name filter for compositions

--- src/apps/api_auth/api/urls.py
+++ src/apps/api_auth/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView
+)
 
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='auth-token'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='auth-token-refresh')
+    path(
+        'token/', TokenObtainPairView.as_view(), name='auth-token'),
+    path(
+        'token/refresh/', TokenRefreshView.as_view(), name='auth-token-refresh'
+    )
 ]
---
+++ src/apps/compositions/api/services.py
@@ -0,0 +1,10 @@
+from django_filters import rest_framework as filter
+from apps.compositions.models import Composition
+
+
+class CompositionNameFilter(filter.FilterSet):
+    name_composition = filter.CharFilter(field_name='name', lookup_expr='in')
+
+    class Meta:
+        model = Composition
+        fields = ('name',)
