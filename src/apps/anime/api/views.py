8678d18e6c6d312d8c36faf9e0d238d8b3c2b9bb
RG1ee <niwows@yandex.ru>
2022-08-12 18:35:35 +0300

    add name filter for anime

--- src/apps/anime/api/views.py
+++ src/apps/anime/api/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets
+from django_filters import rest_framework as filter 
+
 from apps.users.api.permissions import IsStaffOrReadOnly
 
 from apps.anime.api.serializers import AnimeSerializer
 from apps.anime.models import Anime
+from apps.anime.api.services import AnimeFilter
 
 
 class AnimeViewSet(viewsets.ModelViewSet):
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializer
     permission_classes = (IsStaffOrReadOnly,)
+    filter_backends = (filter.DjangoFilterBackend,)
+    filterset_class = AnimeFilter
